98d781e915748f778ddedc8f8f1dc8d474d3fb3d
nosnowowie93347 <nosnowowie@Gmail.com>
2024-01-15 12:21:35 -0800

    f

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,4 +1,4 @@
-const { get } = require("request-promise-native");
+const { fetch } = require("node-fetch");
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,11 +15,16 @@ module.exports = {
     const ppokemon = interaction.options.getString("pokemon");
     const options = {
       url: `https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/pokedex.php?pokemon=${ppokemon}`,
+      method: `GET`,
+       headers: {
+        "Content-Type": "application/vnd.api+json",
+        Accept: "application/vnd.api+json",
+      },
       json: true,
     };
 
     interaction.reply("Fetching Informtion for API").then((msg) => {
-      get(options).then((body) => {
+      fetch(options).then((body) => {
         let embed = new EmbedBuilder()
           .addFields({
             name: body.name,
